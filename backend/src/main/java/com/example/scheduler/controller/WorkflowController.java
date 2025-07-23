015377f283ea9a0b138b64a076f2ec5917e62e79
zhouyongqi <zhouyongqi@zjrcu.com>
2025-07-23 10:17:51 +0800

    1

--- backend/src/main/java/com/example/scheduler/controller/WorkflowController.java
+++ backend/src/main/java/com/example/scheduler/controller/WorkflowController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/workflow")
+@RequestMapping("/api/workflows")
 public class WorkflowController {
 
     @Autowired
