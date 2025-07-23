1349b090907e7ef37abb49b86ebb10ed28f400be
zhouyongqi <zhouyongqi@zjrcu.com>
2025-07-23 10:33:28 +0800

    1

---
+++ backend/src/main/java/com/example/scheduler/filter/RequestLoggingFilter.java
@@ -0,0 +1,37 @@
+package com.example.scheduler.filter;
+
+import com.example.scheduler.wrapper.BufferedRequestWrapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.ContentCachingResponseWrapper;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class RequestLoggingFilter implements Filter {
+
+    private static final Logger logger = LoggerFactory.getLogger(RequestLoggingFilter.class);
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+
+        BufferedRequestWrapper requestWrapper = new BufferedRequestWrapper(req);
+        ContentCachingResponseWrapper responseWrapper = new ContentCachingResponseWrapper(res);
+
+        logger.info("Received request: {} {} from {}", requestWrapper.getMethod(), requestWrapper.getRequestURI(), requestWrapper.getRemoteAddr());
+        logger.info("Request body: {}", requestWrapper.getRequestBody());
+
+        chain.doFilter(requestWrapper, responseWrapper);
+
+        String responseBody = new String(responseWrapper.getContentAsByteArray(), response.getCharacterEncoding());
+        logger.info("Response body: {}", responseBody);
+        responseWrapper.copyBodyToResponse();
+    }
+}
