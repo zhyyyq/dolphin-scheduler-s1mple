3c8966908e407032969c2fa0b3d7d6223fdbd7f1
zhouyongqi <zhouyongqi@zjrcu.com>
2025-07-25 09:44:39 +0800

    1

--- backend/src/main/java/com/example/scheduler/SchedulerApplication.java
+++ backend/src/main/java/com/example/scheduler/SchedulerApplication.java
@@ -1,7 +1,12 @@
 package com.example.scheduler;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.sql.Statement;
 
 @SpringBootApplication
 public class SchedulerApplication {
@@ -10,4 +15,17 @@ public class SchedulerApplication {
         SpringApplication.run(SchedulerApplication.class, args);
     }
 
+    @Bean
+    public ApplicationRunner runner(JdbcTemplate jdbcTemplate) {
+        return args -> {
+            try (Statement statement = jdbcTemplate.getDataSource().getConnection().createStatement()) {
+                statement.executeUpdate("CREATE TABLE IF NOT EXISTS workflows (" +
+                        "uuid VARCHAR(255) PRIMARY KEY," +
+                        "name VARCHAR(255)," +
+                        "online_version VARCHAR(255)," +
+                        "locations VARCHAR(255)" +
+                        ")");
+            }
+        };
+    }
 }
