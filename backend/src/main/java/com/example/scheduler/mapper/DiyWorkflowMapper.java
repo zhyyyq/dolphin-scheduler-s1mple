d4322cf1a73018ed0996eb6e0eef8bd8eeb1158f
zhouyongqi <zhouyongqi@zjrcu.com>
2025-08-19 11:41:03 +0800

    1

---
+++ backend/src/main/java/com/example/scheduler/mapper/DiyWorkflowMapper.java
@@ -0,0 +1,8 @@
+package com.example.scheduler.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.scheduler.model.DiyWorkflow;
+
+public interface DiyWorkflowMapper extends BaseMapper<DiyWorkflow>{
+  
+}
